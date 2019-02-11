46b08cb54d8aa748ace99e44a161660117fc78de
Alexander Harris <alexander.harris@mail.mcgill.ca>
2019-02-11 00:36:01 -0500

    Removed DB URI from VCS

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ module.exports = {
 	ENV: process.env.NODE_ENV || "development",
 	PORT: process.env.PORT || 3000,
 	URL: process.env.BASE_URL || "http://localhost:3000",
-	MONGODB_URI:
-    process.env.MONGODB_URI ||
-    "mongodb://seb:boiii1@ds121455.mlab.com:21455/authboiis",
-	MONGODB_URI_STAGING:
-    process.env.MONGODB_URI_STAGING ||
-    "mongodb://seb:boiii1@ds129045.mlab.com:29045/authboiis-test",
+	MONGODB_URI: process.env.MONGODB_URI,
+	MONGODB_URI_STAGING: process.env.MONGODB_URI_STAGING,
 	JWT_SECRET: process.env.JWT_SECRET || "secret"
 };
