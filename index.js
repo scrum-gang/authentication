c931db4345df0a0c1fa0cf9629130ea219459cde
pepoandra <sebastian.andrade@mail.mcgill.ca>
2019-02-12 16:21:18 -0500

    feature: added CORS support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const restify = require("restify");
 const mongoose = require("mongoose");
 const rjwt = require("restify-jwt-community");
 const config = require("./config");
+const corsMiddleware = require("restify-cors-middleware");
 
 const server = restify.createServer();
 
@@ -13,25 +14,40 @@ const mongoServer = isTestEnv ? new mongoMem.MongoMemoryServer() : null;
 // server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ["/auth"] }));
 server.use(restify.plugins.bodyParser());
 
-if (isTestEnv) {
-	mongoServer.getConnectionString().then((mongoUri) => {
-		const mongooseOpts = {
-			autoReconnect: true,
-			reconnectTries: Number.MAX_VALUE,
-			reconnectInterval: 1000,
-			useNewUrlParser: true
-		};
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ["*"],
+  allowHeaders: ["API-Token"],
+  exposeHeaders: ["API-Token-Expiry"]
+});
 
-		server.listen(config.PORT, () => {
-			mongoose.set("useFindAndModify", false);
-			mongoose.connect(mongoUri, mongooseOpts);
-		});
-	});
+server.pre(cors.preflight);
+server.use(cors.actual);
+
+if (isTestEnv) {
+  mongoServer.getConnectionString().then(mongoUri => {
+    const mongooseOpts = {
+      autoReconnect: true,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 1000,
+      useNewUrlParser: true
+    };
+
+    server.listen(config.PORT, () => {
+      mongoose.set("useFindAndModify", false);
+      mongoose.connect(mongoUri, mongooseOpts);
+    });
+  });
 } else {
-	server.listen(config.PORT, () => {
-		mongoose.set("useFindAndModify", false);
-		mongoose.connect(((process.env.NODE_ENV == "staging") ? config.MONGODB_URI_STAGING : config.MONGODB_URI), { useNewUrlParser: true });
-	});
+  server.listen(config.PORT, () => {
+    mongoose.set("useFindAndModify", false);
+    mongoose.connect(
+      process.env.NODE_ENV == "staging"
+        ? config.MONGODB_URI_STAGING
+        : config.MONGODB_URI,
+      { useNewUrlParser: true }
+    );
+  });
 }
 
 const db = mongoose.connection;
@@ -39,15 +55,15 @@ const db = mongoose.connection;
 db.on("error", err => console.log(err));
 
 db.once("open", () => {
-	require("./routes/users")(server);
-	console.log(`Server started on port ${config.PORT}`);
+  require("./routes/users")(server);
+  console.log(`Server started on port ${config.PORT}`);
 });
 
-function stop () {
-	if (isTestEnv) {
-		mongoServer.stop();
-	}
-	server.close();
+function stop() {
+  if (isTestEnv) {
+    mongoServer.stop();
+  }
+  server.close();
 }
 
 module.exports = server;
