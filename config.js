e19b9191c25dab4ec8027cf4457bbc81a0675316
pepoandra <sebastian.andrade@mail.mcgill.ca>
2019-02-12 15:55:03 -0500

    feature: Added CORS [*] support

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-	ENV: process.env.NODE_ENV || "development",
-	PORT: process.env.PORT || 3000,
-	URL: process.env.BASE_URL || "http://localhost:3000",
-	MONGODB_URI: process.env.MONGODB_URI,
-	MONGODB_URI_STAGING: process.env.MONGODB_URI_STAGING,
-	JWT_SECRET: process.env.JWT_SECRET || "secret"
+  ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || "http://localhost:3000",
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    "mongodb://seb:boiii1@ds121455.mlab.com:21455/authboiis",
+  MONGODB_URI_STAGING: process.env.MONGODB_URI_STAGING,
+  JWT_SECRET: process.env.JWT_SECRET || "secret"
 };
